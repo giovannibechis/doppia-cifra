b22875780a638a2a09438b60de6db9a5ae5a0726
GiovanniBechis <giovanni.bechis@gmail.com>
2022-11-23 18:10:15 +0100

    test wikipedia api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'doppia-cifra';
+
+  ngOnInit(): void {
+    this.fetchAsync();
+  }
+
+  async  doFetch(): Promise<void> {
+
+    const rsp = await fetch(
+      'http://it.wikipedia.org/w/api.php?action=opensearch&limit=10&format=json&search=belotti'
+    );
+    return await rsp.json();
+  }
+
+  async fetchAsync(): Promise<void> {
+    try {
+      const result = await this.doFetch();
+      console.log(result);
+    } catch (err: any) {
+      console.error(err.message);
+    }
+  }
+
 }
